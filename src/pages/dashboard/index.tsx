df4d33ba10aba2edaec36c48dd2a57684d262ba3
ducnnn <ducnn.230288@gmail.com>
2023-10-25 06:50:04 +0700

    add draggabilly

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,24 +1,44 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { t } from 'i18next';
 import Draggabilly from 'draggabilly';
+import dayjs from 'dayjs';
+import 'dayjs/locale/vi';
 const Page = () => {
+  const remainingMonths = (month: number) => {
+    const remaining = [];
+    for (let i = month; i < 12; i++) {
+      remaining.push(i + 1);
+    }
+    return remaining;
+  };
+  const [months, setMonths] = useState(remainingMonths(dayjs().month()));
+
   useEffect(() => {
+    console.log(months);
+    dayjs.locale('vi');
+    let wLeft = 0;
+    let wRight = 0;
+    const left = document.getElementById('left');
+    const right = document.getElementById('right');
     new Draggabilly(document.getElementById('drag')!, {
       axis: 'x',
-    }).on('pointerMove', (_, __, moveVector) => {
-      const left = document.getElementById('left');
-      const right = document.getElementById('right');
-      if (left && right) {
-        if (left.style.flexBasis.indexOf('%')) left.style.flexBasis = left.offsetWidth + 'px';
-        if (right.style.flexBasis.indexOf('%')) right.style.flexBasis = right.offsetWidth + 'px';
-        const width = left.parentElement!.offsetWidth;
-        const p = moveVector.x;
-        left.style.flexBasis = width / 2 + p + 'px';
-        right.style.flexBasis = width / 2 - p + 'px';
-      }
-
-      // document.getElementById('right')
-    });
+    })
+      .on('dragStart', () => {
+        if (left && right) {
+          const width = left.parentElement!.offsetWidth;
+          if (left.style.flexBasis.indexOf('%') > 0) left.style.flexBasis = width / 2 + 'px';
+          if (right.style.flexBasis.indexOf('%') > 0) right.style.flexBasis = width / 2 + 'px';
+          wLeft = parseFloat(left.style.flexBasis.split('px')[0]);
+          wRight = parseFloat(right.style.flexBasis.split('px')[0]);
+        }
+      })
+      .on('dragMove', (_, __, moveVector) => {
+        if (left && right) {
+          const p = moveVector.x;
+          left.style.flexBasis = wLeft + p + 'px';
+          right.style.flexBasis = wRight - p + 'px';
+        }
+      });
   }, []);
   return (
     <Fragment>
@@ -26,7 +46,7 @@ const Page = () => {
         <h1 className="text-3xl text-teal-900 font-bold text-center mb-14 ">{t('routes.auth.login.Welcome')}</h1>
         <div className={'w-full flex relative'}>
           <div id={'left'} className={'overflow-hidden'} style={{ flexBasis: '50%' }}>
-            <table className={'w-full'}>
+            <table className={'w-full min-w-[600px]'}>
               <thead>
                 <tr>
                   <th align={'left'}>Product Release</th>
@@ -51,27 +71,19 @@ const Page = () => {
           </div>
           <div id={'drag'} className={'w-1 h-16 bg-gray-300 cursor-ew-resize hover:bg-red-500 absolute left-1/2'}></div>
           <div id={'right'} className={'overflow-hidden'} style={{ flexBasis: '50%' }}>
-            <table className={'w-full'}>
+            <table className={'w-full min-w-[600px]'}>
               <thead>
                 <tr>
-                  <th align={'left'}>Product Release</th>
-                  <th align={'left'}>Assignee</th>
-                  <th align={'left'}>Status</th>
-                  <th align={'left'}>Priority</th>
-                  <th align={'left'}>Planned Hours</th>
-                  <th align={'left'}>Work Log</th>
+                  {months.map((i, index) => (
+                    <th key={index} align={'left'} className={'capitalize'}>
+                      {dayjs()
+                        .month(i - 1)
+                        .format('MMMM')}
+                    </th>
+                  ))}
                 </tr>
               </thead>
-              <tbody>
-                <tr>
-                  <td>Q-1 Release</td>
-                  <td></td>
-                  <td>In Progress</td>
-                  <td></td>
-                  <td>2</td>
-                  <td>71 days</td>
-                </tr>
-              </tbody>
+              <tbody></tbody>
             </table>
           </div>
         </div>
