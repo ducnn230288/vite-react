09a8c960d38330a063151db0b5050d083b06f23a
ducnguyen <ducnn.230288@gmail.com>
2023-11-08 17:33:44 +0700

    add gantt

--- src/core/gantt/index.tsx
+++ src/core/gantt/index.tsx
@@ -145,7 +145,6 @@ export const Gantt = ({
     let start = dayjs();
     let end = dayjs().add(1, 'months');
     if (data.length && date.total === 0) {
-
       start = data[0].startDate;
       end = data[0].endDate || data[0].startDate.add(1, 'months');
       data.forEach((item) => {
@@ -153,11 +152,7 @@ export const Gantt = ({
         if (item.endDate && item.endDate > end) end = item.endDate;
       });
     }
-    document
-      .querySelectorAll(`#${id.current} .overflow-scroll`)
-      .forEach((e: any) => (e.style.height = maxHeight + 'px'));
     setDate(remainingMonths(start, end));
-
   }, [data]);
 
   useEffect(() => {
@@ -165,11 +160,12 @@ export const Gantt = ({
       (document.querySelector(`#${id.current} .left .head`) as any)!.style.width =
         document.querySelector(`#${id.current} .left .body`)!.clientWidth + getScrollBarWidth() + 'px';
       document.querySelectorAll(`#${id.current} .left tbody > tr:nth-of-type(1) > td`).forEach((e: any, index, arr) => {
-        console.log( e.clientWidth + (arr.length - 1 === index ? getScrollBarWidth() : 0) + 'px');
-        console.log((document.querySelector(`#${id.current} .left thead > tr:nth-of-type(1) > th:nth-of-type(${index + 1})`) as any));
         (document.querySelector(`#${id.current} .left thead > tr:nth-of-type(1) > th:nth-of-type(${index + 1})`) as any)!.style.width =
           e.clientWidth + (arr.length - 1 === index ? getScrollBarWidth() : 0) + 'px';
       });
+      document
+        .querySelectorAll(`#${id.current} .overflow-scroll`)
+        .forEach((e: any) => (e.style.height = maxHeight + 'px'));
     }
   }, [date]);
 
@@ -249,10 +245,10 @@ export const Gantt = ({
     if (e.target.dataset.scrollX)
       document.querySelector(`#${id.current} ${e.target.dataset.scrollX}`)!.scrollTo({ left: e.target.scrollLeft });
   };
-  const NameColumn = ({ name, isDrag = true }: { name: string; isDrag?: boolean }) => (
+  const NameColumn = ({ name }: { name: string; isDrag?: boolean }) => (
     <th align={'left'} className="capitalize border px-4 h-12 text-xs relative truncate">
       {name}
-      {isDrag && <div className="w-0.5 h-12 absolute right-0 top-0 cursor-ew-resize drag"></div>}
+      {/*, isDrag = true {isDrag && <div className="w-0.5 h-12 absolute right-0 top-0 cursor-ew-resize drag"></div>}*/}
     </th>
   );
   const renderSvg = (item: TTask, i: number) => {
@@ -285,9 +281,7 @@ export const Gantt = ({
                 tabIndex={-1}
               ></path>
               <path
-                d={`M ${endLeft + 4.2} ${endTop - 4.5} L ${endLeft - 4.5} ${endTop - 4.5} L ${
-                  endLeft + 0.2
-                } ${endTop} Z`}
+                d={`M ${endLeft + 4.2} ${endTop - 4.5} L ${endLeft - 4.5} ${endTop - 4.5} L ${endLeft + 0.2} ${endTop} Z`}
                 aria-label={item.name}
                 fill={!item.endDate ? 'black' : '#2563eb'}
               ></path>
@@ -356,6 +350,30 @@ export const Gantt = ({
       </div>
     );
   };
+
+  const widthGantt = (year: string, month: string) => (dayjs()
+      .year(parseInt(year))
+      .month(parseInt(month))
+      .endOf('month')
+      .diff(date.obj[year][month][date.obj[year][month].length - 1], 'days') < perRow
+      ? dayjs()
+        .year(parseInt(year))
+        .month(parseInt(month))
+        .endOf('month')
+        .diff(date.obj[year][month][0], 'days') >
+      date.obj[year][month][0].daysInMonth() - (widthMonthYear / widthColumnDay) * perRow
+        ? date.obj[year][month][0].daysInMonth()
+        : dayjs()
+        .year(parseInt(year))
+        .month(parseInt(month))
+        .endOf('month')
+        .diff(date.obj[year][month][0], 'days') + 1
+      : date.obj[year][month][date.obj[year][month].length - 1].diff(
+      date.obj[year][month][0].startOf('month'),
+      'days',
+    ) + perRow) *
+    (widthColumnDay / perRow) +
+    'px';
   return (
     <div id={id.current} className="relative">
       <div className="relative">
@@ -443,32 +461,7 @@ export const Gantt = ({
                           key={index}
                           align={'left'}
                           className={'capitalize border-l border-r border-t px-4 h-6 text-xs'}
-                          style={{
-                            width:
-                              (dayjs()
-                                .year(parseInt(year))
-                                .month(parseInt(month))
-                                .endOf('month')
-                                .diff(date.obj[year][month][date.obj[year][month].length - 1], 'days') < perRow
-                                ? dayjs()
-                                    .year(parseInt(year))
-                                    .month(parseInt(month))
-                                    .endOf('month')
-                                    .diff(date.obj[year][month][0], 'days') >
-                                  date.obj[year][month][0].daysInMonth() - (widthMonthYear / widthColumnDay) * perRow
-                                  ? date.obj[year][month][0].daysInMonth()
-                                  : dayjs()
-                                      .year(parseInt(year))
-                                      .month(parseInt(month))
-                                      .endOf('month')
-                                      .diff(date.obj[year][month][0], 'days') + 1
-                                : date.obj[year][month][date.obj[year][month].length - 1].diff(
-                                    date.obj[year][month][0].startOf('month'),
-                                    'days',
-                                  ) + perRow) *
-                                (widthColumnDay / perRow) +
-                              'px',
-                          }}
+                          style={{width: widthGantt(year, month)}}
                         >
                           {date.obj[year][month][0].format('MMMM')} {year}
                         </th>
